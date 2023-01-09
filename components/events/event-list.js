b7973df17bd8841ee9a2096584c83afd939f83a1
Lange <angelochek9019@gmail.com>
2023-01-08 21:42:16 -0500

    Revert "Styles added to entries."
    This reverts commit fdf3d97762bfeb54652c39aaf964b7016101e3dd.

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,11 +1,10 @@
 import EventItem from "./event-item";
-import classes from "./event-list.module.css";
 
 const EventList = (props) => {
   const { items } = props;
 
   return (
-    <ul className={classes.list}>
+    <ul>
       {items.map((event) => (
         <EventItem
           key={event.id}
